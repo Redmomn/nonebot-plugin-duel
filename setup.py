bff0c23f12fe8e13a42881305dcb7d74194b473e
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-03-22 00:04:25 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nonebot_plugin_duel',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'nonebot-adapter-onebot>=2.0.0',
