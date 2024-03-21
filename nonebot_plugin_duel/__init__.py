8c196163fef9106afbbe2f224b3c55d098250e07
Redmomn <109732988+Redmomn@users.noreply.github.com>
2024-03-22 00:09:25 +0800

    update: 更新插件元数据

--- nonebot_plugin_duel/__init__.py
+++ nonebot_plugin_duel/__init__.py
@@ -6,7 +6,7 @@ from .matcher import dule_req, agree_req, shot_req
 __plugin_meta__ = PluginMetadata(
     name="nonebot-plugin-duel",
     description="决斗插件",
-    usage="",
+    usage="发送关键词`决斗`并@一个人即可和他发起决斗，例如`决斗@阿米娅`",
     type="application",
     homepage="https://github.com/Redmomn/nonebot-plugin-duel",
     supported_adapters={"~onebot.v11"},
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nonebot_plugin_duel',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'nonebot-adapter-onebot>=2.0.0',
